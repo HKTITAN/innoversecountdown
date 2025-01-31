e87696637da2619378d41b8368f0277d0389934b
Aditya <uic.17mca8108@gmail.com>
2025-01-31 23:48:02 +0530

    Some modifications

--- src/pages/socket.ts
+++ src/pages/socket.ts
@@ -1,6 +1,6 @@
 // socket.js (Create this file)
 import io from 'socket.io-client';
 
-const socket = io('https://countdown-timer-em6a-fkgahixo0-aditya-kalias-projects.vercel.app');
+const socket = io('https://countdown-timer-mtsj.onrender.com');
 
 export default socket;
